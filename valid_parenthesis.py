59fe48153564eddc27197dbdc3648d01c2c8be7c
Chase Clark <chase_clark07@hotmail.com>
2025-04-08 19:42:50 -0400

    valid_parenthesis

---
+++ valid_parenthesis.py
@@ -0,0 +1,17 @@
+def is_valid(s: str) -> bool:
+    stack = []
+    dict = {"]": "[", "}": "{", ")": "("}
+    for char in s:
+        if char in dict.values():
+            stack.append(char)
+        elif char in dict.keys():
+            if stack == [] or dict[char] != stack.pop():
+                return False
+        else:
+            return False
+    return stack == []
+
+
+assert is_valid("()") is True
+assert is_valid("(") is False
+assert is_valid("()[]{}") is True
